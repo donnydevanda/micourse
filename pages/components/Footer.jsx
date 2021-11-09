5c4d21fe27c2dcb7dfaaf5b7894683e5f29edb96
Muhammad Donny Devanda <devandadonny@gmail.com>
2021-11-09 19:58:20 +0700

    Next Link Fix

--- pages/components/Footer.jsx
+++ pages/components/Footer.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Link from "next/Link";
+import Link from "next/link";
 import FormEmail from "./FormEmail";
 
 export default function Footer() {
--- pages/components/ListCourses/RenderItem.jsx
+++ pages/components/ListCourses/RenderItem.jsx
@@ -1,4 +1,4 @@
-import Link from "next/Link";
+import Link from "next/link";
 import Image from "next/image";
 import IconPlay from "../../../public/images/ic-play.svg";
 import formatThousand from "../../../helpers/formatThousand";
