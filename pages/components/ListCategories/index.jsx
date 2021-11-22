f0af8183ba5906849935365c002e80da30af989e
Muhammad Donny Devanda <devandadonny@gmail.com>
2021-11-22 15:57:57 +0700

    Categories Fix

--- pages/components/ListCategories/index.jsx
+++ pages/components/ListCategories/index.jsx
@@ -1,10 +1,39 @@
 import Link from "next/link";
 import RenderItem from "./RenderItem";
-import Data from "./Data";
 
 export default function index() {
-  const data = Data;
-
+  const data = [
+    {
+      imageName: "/images/ic-categories/ic-business.svg",
+      name: "Business Development",
+      total: 241,
+    },
+    {
+      imageName: "/images/ic-categories/ic-content.svg",
+      name: "Content Creator",
+      total: 1511,
+    },
+    {
+      imageName: "/images/ic-categories/ic-customer.svg",
+      name: "Customer Relationship",
+      total: 127,
+    },
+    {
+      imageName: "/images/ic-categories/ic-game.svg",
+      name: "Game Development",
+      total: 289,
+    },
+    {
+      imageName: "/images/ic-categories/ic-product.svg",
+      name: "Product Management",
+      total: 1311,
+    },
+    {
+      imageName: "/images/ic-categories/ic-travel.svg",
+      name: "Travel Guidance",
+      total: 303,
+    },
+  ];
   return (
     <div className="container mx-auto px-4 md:pt-24">
       <div className="flex justify-between item-center">
